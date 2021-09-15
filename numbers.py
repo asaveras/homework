a5f5accc6b0dd16601e74e43fe8fcb803327414c
Nadezhda <nadezhdas.rum@yandex.ru>
2021-09-15 20:57:25 +0300

    Изменила всё

--- numbers.py
+++ numbers.py
@@ -1,3 +1,5 @@
+print('Привет!')
 number = input('Напиши число')
 print('За числом',int(number),'следует число',int(number)+1)
 print('Перед числом',int(number),'следует число',int(number)-1)
+print('Bye')
